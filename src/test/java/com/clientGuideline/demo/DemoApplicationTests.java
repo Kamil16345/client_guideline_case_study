a7c536499ad089868674bd0ec8390495dd61062b
Kamil <KamilArkit@interia.pl>
2025-02-16 20:15:30 +0100

    Initial commit.

---
+++ src/test/java/com/clientGuideline/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientGuideline.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
